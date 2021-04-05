8d3674f384569b6f7c4d72d8c61417876b68ce57
Vojtech Mares <iam@vojtechmares.com>
2021-04-05 18:44:19 +0200

    fix(tf cloud): added ondrejsika

--- terraform-cloud/organization.tf
+++ terraform-cloud/organization.tf
@@ -2,3 +2,8 @@ resource "tfe_organization" "vojtechmares" {
   name  = "vojtechmares"
   email = "iam@vojtechmares.com"
 }
+
+resource "tfe_organization_membership" "ondrejsika2vojtechmares" {
+  organization  = tfe_organization.vojtechmares.id
+  email = "ondrej@ondrejsika.com"
+}
