6e82d0d687aa62fa1bba29477ed0dbc46e95c970
Vojtech Mares <iam@vojtechmares.com>
2021-04-05 22:29:04 +0200

    feat(gitlab): added minecraft group and project for 1.17 server

---
+++ gitlab/groups.tf
@@ -0,0 +1,5 @@
+resource "gitlab_group" "minecraft_server" {
+  name        = "Minecraft Server"
+  path        = "minercraft-server"
+  description = "Minecraft Server"
+}
--- gitlab/projects.tf
+++ gitlab/projects.tf
@@ -4,3 +4,10 @@ resource "gitlab_project" "vojtechmares_infra" {
   description  = "My Infrastructure"
   namespace_id = data.gitlab_user.vojtechmares.user_id
 }
+
+resource "gitlab_project" "mc_server" {
+  name         = "Server"
+  path         = "server"
+  description  = "Minecraft Server"
+  namespace_id = gitlab_group.minecraft_server.id
+}
