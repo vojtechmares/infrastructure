31ee506eb1b614875eb541e608e194361a085eec
Vojtech Mares <iam@vojtechmares.com>
2021-04-05 22:36:30 +0200

    feat(gitlab): granted Tomas Grim developer access to Minecraft Server group

--- gitlab/memberships.tf
+++ gitlab/memberships.tf
@@ -3,3 +3,9 @@ resource "gitlab_project_membership" "ondrejsika_to_vojtechmares_infra" {
   user_id      = module.ondrejsika.id
   access_level = "reporter"
 }
+
+resource "gitlab_group_membership" "tomasgrim_to_mc" {
+  group_id     = gitlab_group.minecraft_server.id
+  user_id      = module.tomasgrim.id
+  access_level = "developer"
+}
