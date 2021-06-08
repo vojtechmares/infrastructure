b07136a0e2d701b47dbe79fa0505133e9ff811a1
Vojtech Mares <iam@vojtechmares.com>
2021-06-08 14:45:21 +0200

    deps(core): update digitalocean provider to 2.9.0

--- core/versions.tf
+++ core/versions.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "2.7.0"
+      version = "2.9.0"
     }
     hcloud = {
       source  = "hetznercloud/hcloud"
