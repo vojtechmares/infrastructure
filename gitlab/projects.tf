bb7be0004409cf6c426c62a686dcf84b14b1a946
Vojtech Mares <iam@vojtechmares.com>
2021-06-08 14:32:30 +0200

    feat(gitlab): set project default branch to `main`, apply `terraform fmt`

--- gitlab/projects.tf
+++ gitlab/projects.tf
@@ -1,20 +1,27 @@
+locals {
+  default_branch = "main"
+}
+
 resource "gitlab_project" "vojtechmares_infra" {
-  name         = "Infra"
-  path         = "infra"
-  description  = "My Infrastructure"
-  namespace_id = data.gitlab_user.vojtechmares.user_id
+  name           = "Infra"
+  path           = "infra"
+  description    = "My Infrastructure"
+  namespace_id   = data.gitlab_user.vojtechmares.user_id
+  default_branch = local.default_branch
 }
 
 resource "gitlab_project" "mc_server" {
-  name         = "Server"
-  path         = "server"
-  description  = "Minecraft Server"
-  namespace_id = gitlab_group.minecraft_server.id
+  name           = "Server"
+  path           = "server"
+  description    = "Minecraft Server"
+  namespace_id   = gitlab_group.minecraft_server.id
+  default_branch = local.default_branch
 }
 
 resource "gitlab_project" "mc_discord_bot" {
-  name         = "Discord Bot"
-  path         = "discord-bot"
-  description  = "Minecraft Server Discord Bot for automatic white-listing of new players"
-  namespace_id = gitlab_group.minecraft_server.id
+  name           = "Discord Bot"
+  path           = "discord-bot"
+  description    = "Minecraft Server Discord Bot for automatic white-listing of new players"
+  namespace_id   = gitlab_group.minecraft_server.id
+  default_branch = local.default_branch
 }
