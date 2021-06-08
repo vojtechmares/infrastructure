25417dcb4d30c20c7b158bb06dcf2623efd71019
Vojtech Mares <iam@vojtechmares.com>
2021-06-08 14:39:56 +0200

    feat(gitlab): mirror infra repo to GitHub

---
+++ gitlab/mirroring.tf
@@ -0,0 +1,4 @@
+resource "gitlab_project_mirror" "infra_repo_to_github" {
+  project = "34"
+  url     = "https://vojtechmares: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@github.com/vojtechmares/vojtechmares-infra.git"
+}
