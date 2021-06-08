71f1c002540749895bfcc946e43aae4f95f3c350
Vojtech Mares <iam@vojtechmares.com>
2021-06-08 14:14:56 +0200

    fix(tf cloud): enforce remote TF execution

--- terraform-cloud/workspaces.tf
+++ terraform-cloud/workspaces.tf
@@ -1,19 +1,23 @@
 resource "tfe_workspace" "terraform_cloud" {
   name         = "terraform-cloud"
   organization = tfe_organization.vojtechmares.id
+  execution_mode = "remote"
 }
 
 resource "tfe_workspace" "core" {
   name         = "core"
   organization = tfe_organization.vojtechmares.id
+  execution_mode = "remote"
 }
 
 resource "tfe_workspace" "domain" {
   name         = "domain"
   organization = tfe_organization.vojtechmares.id
+  execution_mode = "remote"
 }
 
 resource "tfe_workspace" "gitlab" {
   name         = "gitlab"
   organization = tfe_organization.vojtechmares.id
+  execution_mode = "remote"
 }
