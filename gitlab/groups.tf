b603cbeef3c2799abb1ca5c597e73446060dc1f2
Vojtech Mares <iam@vojtechmares.com>
2021-06-10 12:40:53 +0200

    feat(gitlab): add statica cloud group and monorepo

--- gitlab/groups.tf
+++ gitlab/groups.tf
@@ -3,3 +3,9 @@ resource "gitlab_group" "minecraft_server" {
   path        = "minercraft-server"
   description = "Minecraft Server"
 }
+
+resource "gitlab_group" "statica_cloud" {
+  name        = "Statica Cloud"
+  path        = "statica-cloud"
+  description = "Statica Cloud"
+}
--- gitlab/projects.tf
+++ gitlab/projects.tf
@@ -32,3 +32,11 @@ resource "gitlab_project" "mc_discord_bot" {
   namespace_id   = gitlab_group.minecraft_server.id
   default_branch = local.default_branch
 }
+
+resource "gitlab_project" "statica_cloud_monorepo" {
+  name           = "Monorepo"
+  path           = "monorepo"
+  description    = "Statica cloud monorepo"
+  namespace_id   = gitlab_group.statica_cloud.id
+  default_branch = local.default_branch
+}
