ce088f76ce8bb96b37fac1ad7576ee2ffe21a4c2
Vojtech Mares <iam@vojtechmares.com>
2021-11-07 23:40:31 +0100

    deps(cloudflare): bump cloudflare provider to 3.4.0

--- cloudflare/versions.tf
+++ cloudflare/versions.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "2.21.0"
+      version = "3.4.0"
     }
     aws = {
       source  = "hashicorp/aws"
