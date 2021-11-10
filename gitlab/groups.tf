8160adcef1e9ebd23b8b3cee148ff9f21c6e1cfe
Vojtech Mares <iam@vojtechmares.com>
2021-11-10 17:32:41 +0100

    refactor(gitlab): delete mc server group and it's projects

--- gitlab/groups.tf
+++ gitlab/groups.tf
@@ -4,12 +4,6 @@ resource "gitlab_group" "mareshq" {
   description = "MaresHQ"
 }
 
-resource "gitlab_group" "minecraft_server" {
-  name        = "Minecraft Server"
-  path        = "minercraft-server"
-  description = "Minecraft Server"
-}
-
 resource "gitlab_group" "statica_cloud" {
   name        = "Statica Cloud"
   path        = "statica-cloud"
--- gitlab/projects.tf
+++ gitlab/projects.tf
@@ -2,22 +2,6 @@ locals {
   default_branch = "main"
 }
 
-resource "gitlab_project" "mc_server" {
-  name           = "Server"
-  path           = "server"
-  description    = "Minecraft Server"
-  namespace_id   = gitlab_group.minecraft_server.id
-  default_branch = local.default_branch
-}
-
-resource "gitlab_project" "mc_discord_bot" {
-  name           = "Discord Bot"
-  path           = "discord-bot"
-  description    = "Minecraft Server Discord Bot for automatic white-listing of new players"
-  namespace_id   = gitlab_group.minecraft_server.id
-  default_branch = local.default_branch
-}
-
 resource "gitlab_project" "statica_cloud_monorepo" {
   name           = "Monorepo"
   path           = "monorepo"
