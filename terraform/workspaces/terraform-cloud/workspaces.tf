509423936c775b9c36390a253dc670216a6b149f
Vojtech Mares <iam@vojtechmares.com>
2021-12-31 20:11:06 +0100

    feat(ws/tf-cloud): all workspaces use TF 1.1.2

--- terraform/workspaces/terraform-cloud/workspaces.tf
+++ terraform/workspaces/terraform-cloud/workspaces.tf
@@ -2,26 +2,26 @@ resource "tfe_workspace" "terraform_cloud" {
   name              = "terraform-cloud"
   organization      = tfe_organization.vojtechmares.id
   execution_mode    = "remote"
-  terraform_version = "1.0.0"
+  terraform_version = "1.1.2"
 }
 
 resource "tfe_workspace" "cloudflare" {
   name              = "cloudflare"
   organization      = tfe_organization.vojtechmares.id
   execution_mode    = "remote"
-  terraform_version = "1.0.0"
+  terraform_version = "1.1.2"
 }
 
 resource "tfe_workspace" "gitlab" {
   name              = "gitlab"
   organization      = tfe_organization.vojtechmares.id
   execution_mode    = "remote"
-  terraform_version = "1.0.0"
+  terraform_version = "1.1.2"
 }
 
 resource "tfe_workspace" "hcloud" {
   name              = "hcloud"
   organization      = tfe_organization.vojtechmares.id
   execution_mode    = "remote"
-  terraform_version = "1.0.0"
+  terraform_version = "1.1.2"
 }
