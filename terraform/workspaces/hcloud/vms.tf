ac305557378761a79eeadd94aa4a4a58576f7250
Vojtech Mares <iam@vojtechmares.com>
2022-01-07 20:32:09 +0100

    feat(ws/hcloud): add gitlab and sentry VMs

---
+++ terraform/workspaces/hcloud/vms.tf
@@ -0,0 +1,46 @@
+resource "hcloud_server" "gitlab" {
+  name        = "gitlab"
+  image       = "ubuntu-20.04"
+  server_type = "cpx31"
+  location    = "fsn1"
+  ssh_keys    = [hcloud_ssh_key.vojtechmares.name]
+  backups     = true
+  user_data   = file("files/docker.cloud-config.yml")
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes = [
+      user_data
+    ]
+  }
+}
+
+output "gitlab_ip" {
+  value = {
+    ipv4 = hcloud_server.gitlab.ipv4_address,
+    ipv6 = hcloud_server.gitlab.ipv6_address,
+  }
+}
+
+resource "hcloud_server" "sentry" {
+  name        = "sentry"
+  image       = "ubuntu-20.04"
+  server_type = "cpx31"
+  location    = "fsn1"
+  ssh_keys    = [hcloud_ssh_key.vojtechmares.name]
+  user_data   = file("files/docker.cloud-config.yml")
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes = [
+      user_data
+    ]
+  }
+}
+
+output "sentry_ip" {
+  value = {
+    ipv4 = hcloud_server.sentry.ipv4_address,
+    ipv6 = hcloud_server.sentry.ipv6_address,
+  }
+}
