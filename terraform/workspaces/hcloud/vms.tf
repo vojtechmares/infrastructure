be70a49cfb033f53ee2c3beefc5d869180d01c87
Vojtech Mares <iam@vojtechmares.com>
2022-02-27 16:02:39 +0100

    feat(hcloud): add gitlab runnner

--- terraform/workspaces/hcloud/vms.tf
+++ terraform/workspaces/hcloud/vms.tf
@@ -21,3 +21,19 @@ output "gitlab_ip" {
     ipv6 = hcloud_server.gitlab.ipv6_address,
   }
 }
+
+resource "hcloud_server" "gitlab_micro_runner" {
+  name        = "gitlab-runner-micro"
+  image       = "ubuntu-20.04"
+  server_type = "cpx11"
+  location    = "fsn1"
+  ssh_keys    = [hcloud_ssh_key.vojtechmares.name]
+  backups     = true
+  user_data   = file("files/docker.cloud-config.yml")
+
+  lifecycle {
+    ignore_changes = [
+      user_data
+    ]
+  }
+}
