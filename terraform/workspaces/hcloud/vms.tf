8246380c486c8e89649e7941c3f7897381805bf0
Vojtech Mares <iam@vojtechmares.com>
2022-03-25 16:12:47 +0100

    feat(hcloud): add tiny VM for Pterodactyl panel

--- terraform/workspaces/hcloud/vms.tf
+++ terraform/workspaces/hcloud/vms.tf
@@ -61,3 +61,20 @@ output "sentry_ip" {
     ipv6 = hcloud_server.sentry.ipv6_address,
   }
 }
+
+# panel.flakame.se
+resource "hcloud_server" "alder" {
+  name        = "alder"
+  image       = "ubuntu-20.04"
+  server_type = "cx11"
+  location    = "fsn1"
+  ssh_keys    = [hcloud_ssh_key.vojtechmares.name]
+  backups     = true
+}
+
+output "alder_ip" {
+  value = {
+    ipv4 = hcloud_server.alder.ipv4_address,
+    ipv6 = hcloud_server.alder.ipv6_address,
+  }
+}
