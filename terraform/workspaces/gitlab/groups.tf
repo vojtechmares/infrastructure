6661220f2c2a42c7a332686b89c6351c49412492
Vojtech Mares <iam@vojtechmares.com>
2022-04-10 23:00:32 +0200

    feat(gitlab): add Infrastructure and GitOps

--- terraform/workspaces/gitlab/groups.tf
+++ terraform/workspaces/gitlab/groups.tf
@@ -18,8 +18,46 @@ resource "gitlab_group" "mareshq_terraform_modules" {
   parent_id = gitlab_group.mareshq.id
 }
 
-resource "gitlab_group" "heavenrp" {
-  name        = "HeavenRP"
-  path        = "heavenrp"
-  description = "HeavenRP"
+resource "gitlab_group" "infrastructure" {
+  name        = "Infrastructure"
+  path        = "infrastructure"
+  description = "Infrastructure"
+}
+
+resource "gitlab_group" "infrastructure_mareshq" {
+  name        = "MaresHQ"
+  path        = "mareshq"
+  description = "MaresHQ Infrastructure"
+
+  parent_id = gitlab_group.infrastructure.id
+}
+
+resource "gitlab_group" "infrastructure_staticahq" {
+  name        = "StaticaHQ"
+  path        = "staticahq"
+  description = "StaticaHQ Infrastructure"
+
+  parent_id = gitlab_group.infrastructure.id
+}
+
+resource "gitlab_group" "gitops" {
+  name        = "GitOps"
+  path        = "gitops"
+  description = "GitOps"
+}
+
+resource "gitlab_group" "gitops_mareshq" {
+  name        = "MaresHQ"
+  path        = "mareshq"
+  description = "MaresHQ GitOps"
+
+  parent_id = gitlab_group.gitops.id
+}
+
+resource "gitlab_group" "gitops_skautdevs" {
+  name        = "SkautDevs"
+  path        = "skautdevs"
+  description = "SkautDevs GitOps"
+
+  parent_id = gitlab_group.gitops.id
 }
