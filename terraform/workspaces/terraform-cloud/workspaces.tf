f3fc61979dc53277490d5e9ef6cc1757139d291a
Vojtech Mares <iam@vojtechmares.com>
2022-11-14 23:03:49 +0100

    feat(tf cloud): bump tf version to 1.3.4 in all workspaces

--- terraform/workspaces/terraform-cloud/workspaces.tf
+++ terraform/workspaces/terraform-cloud/workspaces.tf
@@ -2,40 +2,40 @@ resource "tfe_workspace" "terraform_cloud" {
   name              = "terraform-cloud"
   organization      = tfe_organization.vojtechmares.id
   execution_mode    = "remote"
-  terraform_version = "1.2.2"
+  terraform_version = "1.3.4"
 }
 
 resource "tfe_workspace" "cloudflare" {
   name              = "cloudflare"
   organization      = tfe_organization.vojtechmares.id
   execution_mode    = "remote"
-  terraform_version = "1.2.2"
+  terraform_version = "1.3.4"
 }
 
 resource "tfe_workspace" "gitlab" {
   name              = "gitlab"
   organization      = tfe_organization.vojtechmares.id
   execution_mode    = "remote"
-  terraform_version = "1.2.2"
+  terraform_version = "1.3.4"
 }
 
 resource "tfe_workspace" "hcloud" {
   name              = "hcloud"
   organization      = tfe_organization.vojtechmares.id
   execution_mode    = "remote"
-  terraform_version = "1.2.2"
+  terraform_version = "1.3.4"
 }
 
 resource "tfe_workspace" "aws" {
   name              = "aws"
   organization      = tfe_organization.vojtechmares.id
   execution_mode    = "remote"
-  terraform_version = "1.1.8"
+  terraform_version = "1.3.4"
 }
 
 resource "tfe_workspace" "do" {
   name              = "do"
   organization      = tfe_organization.vojtechmares.id
   execution_mode    = "remote"
-  terraform_version = "1.2.2"
+  terraform_version = "1.3.4"
 }
