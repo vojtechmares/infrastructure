791024022a7b00cdea996125b7127ddb160e7394
Vojtech Mares <iam@vojtechmares.com>
2022-12-05 18:27:44 +0100

    refactor(tf-cloud): change exec mode and terraform version

--- terraform/workspaces/terraform-cloud/workspaces.tf
+++ terraform/workspaces/terraform-cloud/workspaces.tf
@@ -1,41 +1,41 @@
 resource "tfe_workspace" "terraform_cloud" {
   name              = "terraform-cloud"
   organization      = tfe_organization.vojtechmares.id
-  execution_mode    = "remote"
-  terraform_version = "1.3.4"
+  execution_mode    = "local"
+  terraform_version = "~>1.3.4"
 }
 
 resource "tfe_workspace" "cloudflare" {
   name              = "cloudflare"
   organization      = tfe_organization.vojtechmares.id
-  execution_mode    = "remote"
-  terraform_version = "1.3.4"
+  execution_mode    = "local"
+  terraform_version = "~>1.3.4"
 }
 
 resource "tfe_workspace" "gitlab" {
   name              = "gitlab"
   organization      = tfe_organization.vojtechmares.id
-  execution_mode    = "remote"
-  terraform_version = "1.3.4"
+  execution_mode    = "local"
+  terraform_version = "~>1.3.4"
 }
 
 resource "tfe_workspace" "hcloud" {
   name              = "hcloud"
   organization      = tfe_organization.vojtechmares.id
-  execution_mode    = "remote"
-  terraform_version = "1.3.4"
+  execution_mode    = "local"
+  terraform_version = "~>1.3.4"
 }
 
 resource "tfe_workspace" "aws" {
   name              = "aws"
   organization      = tfe_organization.vojtechmares.id
-  execution_mode    = "remote"
-  terraform_version = "1.3.4"
+  execution_mode    = "local"
+  terraform_version = "~>1.3.4"
 }
 
 resource "tfe_workspace" "do" {
   name              = "do"
   organization      = tfe_organization.vojtechmares.id
-  execution_mode    = "remote"
-  terraform_version = "1.3.4"
+  execution_mode    = "local"
+  terraform_version = "~>1.3.4"
 }
