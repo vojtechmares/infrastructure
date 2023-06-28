ab67d4a3740b09de838ab3c57665bf552256d441
Vojtech Mares <iam@vojtechmares.com>
2023-06-28 17:39:57 +0200

    feat(prod/compute): add bastion machine

--- terraform/environments/prod/compute/network.tf
+++ terraform/environments/prod/compute/network.tf
@@ -10,6 +10,19 @@ resource "hcloud_network_subnet" "standalone_machines" {
   ip_range     = cidrsubnet(hcloud_network.private.ip_range, 12, 1)
 }
 
+resource "hcloud_network_subnet" "kiwi_k8s_nodes" {
+  network_id   = hcloud_network.private.id
+  type         = "cloud"
+  network_zone = "eu-central"
+  ip_range     = cidrsubnet(hcloud_network.private.ip_range, 12, 128)
+}
+
+resource "hcloud_server_network" "bastion_to_private" {
+  server_id = hcloud_server.bastion.id
+  subnet_id = hcloud_network_subnet.standalone_machines.id
+  ip        = cidrhost(hcloud_network_subnet.standalone_machines.ip_range, 1)
+}
+
 resource "hcloud_server_network" "postgres_to_private" {
   server_id = hcloud_server.postgres.id
   subnet_id = hcloud_network_subnet.standalone_machines.id
