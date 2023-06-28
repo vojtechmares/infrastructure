aa0f5cef0d178a8220f36020b28741d7dd8240d7
Vojtech Mares <iam@vojtechmares.com>
2023-06-28 17:39:02 +0200

    refactor(prod/compute): split vms to subnets
    Subnets (machine groups)
    - standalone machines
    - kiwi k8s cluster

--- terraform/environments/prod/compute/network.tf
+++ terraform/environments/prod/compute/network.tf
@@ -3,29 +3,29 @@ resource "hcloud_network" "private" {
   ip_range = "172.16.0.0/12"
 }
 
-resource "hcloud_network_subnet" "cloud_private" {
+resource "hcloud_network_subnet" "standalone_machines" {
   network_id   = hcloud_network.private.id
   type         = "cloud"
   network_zone = "eu-central"
-  ip_range     = "172.16.1.0/24"
+  ip_range     = cidrsubnet(hcloud_network.private.ip_range, 12, 1)
 }
 
 resource "hcloud_server_network" "postgres_to_private" {
   server_id = hcloud_server.postgres.id
-  subnet_id = hcloud_network_subnet.cloud_private.id
-  ip        = cidrhost(hcloud_network_subnet.cloud_private.ip_range, 1)
+  subnet_id = hcloud_network_subnet.standalone_machines.id
+  ip        = cidrhost(hcloud_network_subnet.standalone_machines.ip_range, 2)
 }
 
 resource "hcloud_server_network" "prometheus_to_private" {
   server_id = hcloud_server.prometheus.id
-  subnet_id = hcloud_network_subnet.cloud_private.id
-  ip        = cidrhost(hcloud_network_subnet.cloud_private.ip_range, 2)
+  subnet_id = hcloud_network_subnet.standalone_machines.id
+  ip        = cidrhost(hcloud_network_subnet.standalone_machines.ip_range, 3)
 }
 
 resource "hcloud_server_network" "kiwi_k8s_nodes_to_private" {
   count = length(hcloud_server.kiwi_k8s_nodes)
 
   server_id = hcloud_server.kiwi_k8s_nodes[count.index].id
-  subnet_id = hcloud_network_subnet.cloud_private.id
-  ip        = cidrhost(hcloud_network_subnet.cloud_private.ip_range, count.index + 3) // + 3 because 1 and 2 are already taken, 0 is reserved
+  subnet_id = hcloud_network_subnet.kiwi_k8s_nodes.id
+  ip        = cidrhost(hcloud_network_subnet.kiwi_k8s_nodes.ip_range, count.index + 1)
 }
