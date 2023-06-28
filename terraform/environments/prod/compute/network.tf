4cb890613ceace436d5274a9c568f61681777652
Vojtech Mares <iam@vojtechmares.com>
2023-06-28 08:25:12 +0200

    feat(prod/compute): add private network

---
+++ terraform/environments/prod/compute/network.tf
@@ -0,0 +1,31 @@
+resource "hcloud_network" "private" {
+  name     = "private"
+  ip_range = "172.16.0.0/12"
+}
+
+resource "hcloud_network_subnet" "cloud_private" {
+  network_id   = hcloud_network.private.id
+  type         = "cloud"
+  network_zone = "eu-central"
+  ip_range     = "172.16.1.0/24"
+}
+
+resource "hcloud_server_network" "postgres_to_private" {
+  server_id = hcloud_server.postgres.id
+  subnet_id = hcloud_network_subnet.cloud_private.id
+  ip        = cidrhost(hcloud_network_subnet.cloud_private.ip_range, 1)
+}
+
+resource "hcloud_server_network" "prometheus_to_private" {
+  server_id = hcloud_server.prometheus.id
+  subnet_id = hcloud_network_subnet.cloud_private.id
+  ip        = cidrhost(hcloud_network_subnet.cloud_private.ip_range, 2)
+}
+
+resource "hcloud_server_network" "kiwi_k8s_nodes_to_private" {
+  count = length(hcloud_server.kiwi_k8s_nodes)
+
+  server_id = hcloud_server.kiwi_k8s_nodes[count.index].id
+  subnet_id = hcloud_network_subnet.cloud_private.id
+  ip        = cidrhost(hcloud_network_subnet.cloud_private.ip_range, count.index + 3) // + 3 because 1 and 2 are already taken, 0 is reserved
+}
