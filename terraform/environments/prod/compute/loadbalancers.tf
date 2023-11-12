a1ac8b36733fa4f2d0e3e6b19fb0c3209f7143ad
Vojtech Mares <iam@vojtechmares.com>
2023-11-12 17:31:19 +0100

    refactor(terraform/prod): have a dedicated lb for ha control-plane

--- terraform/environments/prod/compute/loadbalancers.tf
+++ terraform/environments/prod/compute/loadbalancers.tf
@@ -1,12 +1,3 @@
-resource "hcloud_load_balancer" "mallard" {
-  name               = "mallard"
-  load_balancer_type = "lb11"
-  location           = "fsn1"
-  labels = {
-    "env" = "production"
-  }
-}
-
 locals {
   cthulhu_node_ips = [
     "168.119.35.142",
@@ -15,7 +6,20 @@ locals {
   ]
 }
 
-resource "hcloud_load_balancer_target" "k8s_cthulhu_nodes" {
+# mallard
+
+resource "hcloud_load_balancer" "mallard" {
+  name               = "mallard"
+  load_balancer_type = "lb11"
+  location           = "fsn1"
+  labels = {
+    "env"    = "production"
+    "target" = "k8s.control-plane"
+    "k8s"    = "cthulhu"
+  }
+}
+
+resource "hcloud_load_balancer_target" "mallard_k8s_cthulhu_nodes" {
   count = length(local.cthulhu_node_ips)
 
   type             = "ip"
@@ -23,39 +27,78 @@ resource "hcloud_load_balancer_target" "k8s_cthulhu_nodes" {
   load_balancer_id = hcloud_load_balancer.mallard.id
 }
 
-resource "hcloud_load_balancer_service" "k8s_cthulhu_nodes_tcp_80" {
-  load_balancer_id = hcloud_load_balancer.mallard.id
-  protocol         = "tcp"
-  listen_port      = 80
-  destination_port = 80
-}
-
-resource "hcloud_load_balancer_service" "k8s_cthulhu_nodes_tcp_443" {
-  load_balancer_id = hcloud_load_balancer.mallard.id
-  protocol         = "tcp"
-  listen_port      = 443
-  destination_port = 443
-}
-
-resource "hcloud_load_balancer_service" "k8s_cthulhu_nodes_tcp_6443" {
+resource "hcloud_load_balancer_service" "mallard_k8s_cthulhu_nodes_tcp_6443" {
   load_balancer_id = hcloud_load_balancer.mallard.id
   protocol         = "tcp"
   listen_port      = 6443
   destination_port = 6443
 }
 
-resource "cloudflare_record" "shrike_lb_vxm_cz" {
+resource "cloudflare_record" "mallard_lb_vxm_cz" {
   zone_id = local.vxm_cz_zone_id
-  name    = "shrike.lb"
+  name    = "mallard.lb"
   value   = hcloud_load_balancer.mallard.ipv4
   type    = "A"
   proxied = false
 }
 
-resource "cloudflare_record" "shrike_lb_vxm_cz_v6" {
+resource "cloudflare_record" "mallard_lb_vxm_cz_v6" {
   zone_id = local.vxm_cz_zone_id
-  name    = "shrike.lb"
+  name    = "mallard.lb"
   value   = hcloud_load_balancer.mallard.ipv6
+  type    = "AAAA"
+  proxied = false
+}
+
+# chamios
+
+resource "hcloud_load_balancer" "chamois" {
+  name               = "chamois"
+  load_balancer_type = "lb11"
+  location           = "fsn1"
+  labels = {
+    "env"    = "production"
+    "target" = "k8s.ingress"
+    "k8s"    = "cthulhu"
+  }
+}
+
+resource "hcloud_load_balancer_target" "chamois_k8s_cthulhu_nodes" {
+  count = length(local.cthulhu_node_ips)
+
+  type             = "ip"
+  ip               = local.cthulhu_node_ips[count.index]
+  load_balancer_id = hcloud_load_balancer.chamois.id
+}
+
+resource "hcloud_load_balancer_service" "chamois_k8s_cthulhu_nodes_tcp_80" {
+  load_balancer_id = hcloud_load_balancer.chamois.id
+  protocol         = "tcp"
+  listen_port      = 80
+  destination_port = 32080
+  proxyprotocol    = true
+}
+
+resource "hcloud_load_balancer_service" "chamois_k8s_cthulhu_nodes_tcp_443" {
+  load_balancer_id = hcloud_load_balancer.chamois.id
+  protocol         = "tcp"
+  listen_port      = 443
+  destination_port = 32443
+  proxyprotocol    = true
+}
+
+resource "cloudflare_record" "chamois_lb_vxm_cz" {
+  zone_id = local.vxm_cz_zone_id
+  name    = "chamois.lb"
+  value   = hcloud_load_balancer.chamois.ipv4
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "chamois_lb_vxm_cz_v6" {
+  zone_id = local.vxm_cz_zone_id
+  name    = "chamois.lb"
+  value   = hcloud_load_balancer.chamois.ipv6
   type    = "AAAA"
   proxied = false
 }
