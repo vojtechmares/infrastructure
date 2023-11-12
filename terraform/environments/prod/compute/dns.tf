b47247f66d8078b0153cb9e0e41f0848a0a9b5ed
Vojtech Mares <iam@vojtechmares.com>
2023-11-12 11:47:03 +0100

    feat(terraform/prod): add records for cthulhu k8s nodes

--- terraform/environments/prod/compute/dns.tf
+++ terraform/environments/prod/compute/dns.tf
@@ -5,3 +5,28 @@ resource "cloudflare_record" "cthulhu_k8s_vxm_cz" {
   type    = "A"
   proxied = false
 }
+
+# Cthulhu nodes DNS records
+resource "cloudflare_record" "zebra_vxm_cz" {
+  zone_id = local.vxm_cz_zone_id
+  name    = "zebra"
+  value   = "116.202.114.83"
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "camel_vxm_cz" {
+  zone_id = local.vxm_cz_zone_id
+  name    = "camel"
+  value   = "116.202.114.83"
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "raven_vxm_cz" {
+  zone_id = local.vxm_cz_zone_id
+  name    = "raven"
+  value   = "168.119.35.142"
+  type    = "A"
+  proxied = false
+}
