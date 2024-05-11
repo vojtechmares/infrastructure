844bfed86277325b5959a953fb682d2550e1faed
Vojtech Mares <iam@vojtechmares.com>
2024-05-11 18:21:29 +0200

    refactor(terraform-cloud): use tfe_workspace_settings and bump tf versions

--- workspaces/terraform-cloud/workspaces.tf
+++ workspaces/terraform-cloud/workspaces.tf
@@ -1,27 +1,43 @@
 resource "tfe_workspace" "terraform_cloud" {
   name              = "terraform-cloud"
   organization      = data.tfe_organization.vojtechmares.name
-  execution_mode    = "local"
-  terraform_version = "~>1.3.4"
+  terraform_version = "~>1.8.3"
+}
+
+resource "tfe_workspace_settings" "terraform_cloud" {
+  workspace_id   = tfe_workspace.terraform_cloud.id
+  execution_mode = "local"
 }
 
 resource "tfe_workspace" "dns" {
   name              = "dns"
   organization      = data.tfe_organization.vojtechmares.name
-  execution_mode    = "local"
-  terraform_version = "~>1.3.4"
+  terraform_version = "~>1.8.3"
+}
+
+resource "tfe_workspace_settings" "dns" {
+  workspace_id   = tfe_workspace.dns.id
+  execution_mode = "local"
 }
 
 resource "tfe_workspace" "vm" {
   name              = "vm"
   organization      = data.tfe_organization.vojtechmares.name
-  execution_mode    = "local"
   terraform_version = "~>1.3.4"
+}
+
+resource "tfe_workspace_settings" "vm" {
+  workspace_id   = tfe_workspace.vm.id
+  execution_mode = "local"
 }
 
 resource "tfe_workspace" "backup_storage" {
   name              = "backup-storage"
   organization      = data.tfe_organization.vojtechmares.name
-  execution_mode    = "local"
   terraform_version = "~>1.3.4"
+}
+
+resource "tfe_workspace_settings" "backup_storage" {
+  workspace_id   = tfe_workspace.backup_storage.id
+  execution_mode = "local"
 }
