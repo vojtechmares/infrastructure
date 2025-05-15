3db529ee534a07411fc3ee97638ebac993255acb
Vojtěch Mareš <vojtech@mares.cz>
2025-05-15 17:08:42 +0200

    chore(deps): downgrade terraform provider cloudflare for dns workspasce to <5.0.0
    Signed-off-by: Vojtěch Mareš <vojtech@mares.cz>

--- workspaces/dns/versions.tf
+++ workspaces/dns/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "< 6.0.0"
+      version = "< 5.0.0"
     }
     aws = {
       source  = "hashicorp/aws"
